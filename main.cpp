0a35e733f7cab5a28f02d7b99d981581ab9bc3d3
Henryawd345 <henrygoodboy2000@gmail.com>
2025-12-10 20:57:15 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <map>
+using namespace std;
 
 int main () {
+    map<string, int> traffic;
+
+    ifstream fin("210-final-1-FA25.txt");
+    if (!fin)
+    {
+        cout << "Error opening file.\n";
+        return 1;
+    }
+    
+
 
 
     return 0;
