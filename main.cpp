d161e3ca6e1c8ce9533791ed883199260ed07409
Henryawd345 <henrygoodboy2000@gmail.com>
2025-12-10 21:04:54 -0800

    testing milestone 1

--- main.cpp
+++ main.cpp
@@ -8,13 +8,24 @@ int main () {
     map<string, int> traffic;
 
     ifstream fin("210-final-1-FA25.txt");
-    if (!fin)
-    {
+    if (!fin){
         cout << "Error opening file.\n";
         return 1;
     }
     
+    string origin;
+    string dest;
 
+    while (fin >> origin >> dest){
+        traffic[origin]++;
+        traffic[dest]++;
+    }
+    
+    cout << "All airport traffic counts:\n";
+    for (const auto &entry : traffic){
+        cout << entry.first << " " << entry.second << "\n";
+    }
+    
 
 
     return 0;
